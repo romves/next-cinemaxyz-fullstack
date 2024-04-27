1371ba02f5cd4949abc262a0f4b4a741f317da49
Naufal Romero <nofalrmz@gmail.com>
2024-04-27 23:37:12 +0700

    no localhost

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance =
   process.env.NODE_ENV === "production"
     ? axios.create({
-        baseURL: `${process.env.NEXT_PUBLIC_API_URL}api`,
+        baseURL: `api`,
       })
     : axios.create({
         baseURL: "http://localhost:3000/api",

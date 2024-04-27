b626297480174fa01531fdaa03c3fa8890c613bf
Naufal Romero <nofalrmz@gmail.com>
2024-04-27 23:44:54 +0700

    x

--- src/components/NowShowing.tsx
+++ src/components/NowShowing.tsx
@@ -1,9 +1,9 @@
 import { Movie } from "@prisma/client";
 import MovieCard from "./ui/MovieCard";
-import { fetchMovies } from "@/service/movies";
 
 const NowShowing = async () => {
-  const { data: movies } = await fetchMovies();
+  const res = await fetch("http://localhost:3000/api/movies/now-showing");
+  const movies = await res.json();
 
   if (!movies || movies.length == 0) return <section>Movies not found</section>;
 

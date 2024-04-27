6768c6f94f74b74d46976d5e1681c11e9ca52c08
Naufal Romero <nofalrmz@gmail.com>
2024-04-27 23:08:03 +0700

    add api url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-export const axiosInstance =
-  process.env.NODE_ENV === "production"
-    ? axios.create({
-        baseURL: "/api",
-      })
-    : axios.create({
-        baseURL: "http://localhost:3000/api",
-      });
+export const axiosInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+});

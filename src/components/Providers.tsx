9ccef05607b7c312489cfe7502843f9556c80354
rmvs <nofalrmz@gmail.com>
2024-09-16 21:08:14 +0700

    fix: remove devtools

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
 
 interface LayoutProps {
@@ -11,7 +10,7 @@ interface LayoutProps {
 const Providers: FC<LayoutProps> = ({ children }) => {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>{children}<ReactQueryDevtools /></QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
 

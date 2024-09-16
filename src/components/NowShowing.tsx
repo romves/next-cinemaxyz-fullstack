3ab5fc3444dca1f6d13b8fbc2d8cf19c81342bc7
rmvs <nofalrmz@gmail.com>
2024-09-16 21:08:45 +0700

    fix: remove slice

--- src/components/NowShowing.tsx
+++ src/components/NowShowing.tsx
@@ -16,7 +16,7 @@ const NowShowing = async () => {
         <section>
             <h1 className="text-4xl font-bold">Now Showing</h1>
             <div className="flex gap-3 py-2 overflow-auto">
-                {movies?.slice(0,1).map((movie: Movie) => (
+                {movies?.map((movie: Movie) => (
                     <MovieCard
                         key={movie.id}
                         movie={movie}
